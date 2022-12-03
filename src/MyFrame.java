02fcc29bf6344fd7b3ceb66367ce9cf61bec20c9
arek <bajurski@hotmail.com>
2022-12-03 13:03:54 +0100

    Empty MyForm shows up.

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,7 +4,13 @@ import java.awt.event.*;
 public class MyFrame extends JFrame implements ActionListener{
 
     public MyFrame() {
+        setTitle("Registration Form");
+        setBounds(300, 90, 900, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
 
+
+        setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
