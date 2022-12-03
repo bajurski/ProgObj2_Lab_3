21c784a0f169e9af188d5da3fefa1359c5044a02
arek <bajurski@hotmail.com>
2022-12-03 13:06:34 +0100

    Empty MyForm shows up.

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,8 +4,8 @@ import java.awt.event.*;
 public class MyFrame extends JFrame implements ActionListener{
 
     public MyFrame() {
-        setTitle("Registration Form");
-        setBounds(300, 90, 900, 600);
+        setTitle("Show multiplication table.");
+        setBounds(200, 90, 1000, 700);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
 
