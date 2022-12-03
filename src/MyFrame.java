fc85cdb0778066a7c6ea896923105bf00e5b92f0
arek <bajurski@hotmail.com>
2022-12-03 12:56:04 +0100

    stubs for Main and MyFrame created.

---
+++ src/MyFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class MyFrame extends JFrame implements ActionListener{
+
+    public MyFrame() {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
